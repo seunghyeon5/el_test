ce95197080f7ea75c346a434fecad798f6c9d442
seunghyeon5 <ilralz25@naver.com>
2023-07-27 19:41:48 +0900

    ecilpse_parc

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -1,6 +1,6 @@
 //문서에서 일치하는 요소를 찾아서 텍스트를 바꿔주는 코드
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+/*let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';*/
 
 //연산자 연습 코드
 function multiply(a, b) {
@@ -12,10 +12,10 @@ function multiply(a, b) {
 let imgChange = document.querySelector('img');
 imgChange.onclick = function() {
 	let mySrc = imgChange.getAttribute('src'); //getAttrubute는 해당 요소에 지정된 값을 반환
-	if(mySrc === 'images/test_image.jfif'){
-		imgChange.setAttribute ('src', 'images/image.png') //setAtrribute는 지정된 요소의 속성값을 설정
-	}else{
-		imgChange.setAttribute ('src', 'images/test_image.jfif')
+	if (mySrc === 'images/test_image.jfif') {
+		imgChange.setAttribute('src', 'images/image.png') //setAtrribute는 지정된 요소의 속성값을 설정
+	} else {
+		imgChange.setAttribute('src', 'images/test_image.jfif')
 	}
 };
 
@@ -25,7 +25,7 @@ let myHeading = document.querySelector('h1')
 
 function setUserName() {
 	let myName = prompt('Please enter your name.'); //prompt는 사용자에게 데이터를 입력하길 요청
-	if(!myName) {
+	if (!myName) {
 		setUserName();
 	} else {
 		localStorage.setItem('name', myName)
@@ -33,9 +33,9 @@ function setUserName() {
 	}
 };
 
-if(!localStorage.getItem('name')){
-		setUserName();
-	} else {
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
 	let storedName = localStorage.getItem('name');
 	myHeading.textContent = '강아지 소개, ' + storedName;
 } // 이름이 지정되지 않았을 경우 이름 데이터를 남기게 설정
